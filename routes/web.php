805a7e4df96a65ae38d904974d39819fb3c1970e
Oleg Nesterov <NZOlegok@gmail.com>
2017-08-02 13:19:36 +1200

    +Task class

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TaskController@home');
+
+Route::get('tasks/create', 'TaskController@create');
+Route::post('tasks', 'TaskController@store');
